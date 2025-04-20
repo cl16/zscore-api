f702b7ec76ae55df11d17ebed80297d12dc3b69e
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2025-04-20 11:29:30 -0400

    initial commit on pub reqs

--- src/main/java/com/zscore_api/zscore_api/repository/PublicationRepository.java
+++ src/main/java/com/zscore_api/zscore_api/repository/PublicationRepository.java
@@ -4,8 +4,10 @@ import com.zscore_api.zscore_api.entity.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PublicationRepository extends CrudRepository<Publication, Integer>,
         QuerydslPredicateExecutor<Publication>,
-        JpaRepository<Publication, Integer> {
+        JpaRepository<Publication, Integer>,
+        PagingAndSortingRepository<Publication, Integer> {
 }
