5faccdf9bb12dc7e10e9f092055f0bb13b43d3e5
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2025-05-03 12:25:55 -0400

    adds game service constants

--- src/main/java/com/zscore_api/zscore_api/service/GameService.java
+++ src/main/java/com/zscore_api/zscore_api/service/GameService.java
@@ -15,8 +15,12 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
-    Set<String> validRequestParams = new HashSet<>(Arrays.asList("title", "titleContains"));
-    Set<String> sortArgs = new HashSet<>(Arrays.asList("gameId", "title"));
+    private final String ID_STRING = "gameId";
+    private final String TITLE_STRING = "title";
+    private final String TITLE_CONTAINS_STRING = "titleContains";
+
+    Set<String> validRequestParams = new HashSet<>(Arrays.asList(TITLE_STRING, TITLE_CONTAINS_STRING));
+    Set<String> sortArgs = new HashSet<>(Arrays.asList(ID_STRING, TITLE_STRING));
 
     public Iterable<Game> getAllGames(Map<String, String> params, Pageable pageable) {
         ParamValidator.validatePagingAndSortingArgs(params, sortArgs);
@@ -34,17 +38,23 @@ public class GameService {
         ParamValidator.validateDomainRequestParams(params, validRequestParams);
         this.validateRequestParamLogicRules(params);
 
-        if (params.containsKey("title")) {
-            return gameRepository.findByTitle(params.get("title"), pageable);
+        if (params.containsKey(TITLE_STRING)) {
+            return gameRepository.findByTitle(params.get(TITLE_STRING), pageable);
         } else {
-            return gameRepository.findByTitleContains(params.get("titleContains"), pageable);
+            return gameRepository.findByTitleContains(params.get(TITLE_CONTAINS_STRING), pageable);
         }
     }
 
     private void validateRequestParamLogicRules(Map<String, String> params) {
         Set<String> nonPagingAndSortingParams = ParamValidator.nonPagingAndSortingParams(params.keySet());
         if (nonPagingAndSortingParams.size() > 1) {
-            throw new IllegalArgumentException("Invalid request parameters: only 1 allowed from title, titleContains");
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Invalid request parameters: only 1 allowed from %s, %s",
+                            TITLE_STRING,
+                            TITLE_CONTAINS_STRING
+                    )
+            );
         }
     }
 }
